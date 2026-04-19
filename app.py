a8c0d486a8f3abb6f1dfc707beb853f72499444f
malvis Anochie <malvisanochie97@gmail.com>
2026-04-19 11:39:58 +0100

    fix: use compatible streamlit image width parameter

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
     img_array = np.array(image)
 
